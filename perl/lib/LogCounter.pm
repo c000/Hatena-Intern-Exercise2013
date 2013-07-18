4158e2521a0da61182d9d45eb6946063d1cdbf77
c000 <chikusat@hotmail.com>
2013-07-18 11:50:41 +0900

    Implement LogCounter

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -1,6 +1,7 @@
 package LogCounter;
 use strict;
 use warnings;
+use Data::Dumper;
 
 sub new {
     my ($class, $logs) = @_;
@@ -8,9 +9,31 @@ sub new {
 };
 
 sub group_by_user {
+    my ($self) = @_;
+    my $group_hash = {};
+    foreach my $log (@{$self->{logs}}) {
+        my $user = "guest";
+        if ($log->{user}) {
+            $user = $log->{user};
+        }
+        if (exists $group_hash->{$user}) {
+            push @{$group_hash->{$user}}, $log;
+        } else {
+            $group_hash->{$user} = [$log];
+        }
+    }
+    return $group_hash;
 }
 
 sub count_error {
+    my ($self) = @_;
+    my $error_count = 0;
+    foreach my $log (@{$self->{logs}}) {
+        if ($log->{status} =~ /5\d{2}/) {
+            $error_count += 1;
+        }
+    }
+    return $error_count;
 }
 
 1;
