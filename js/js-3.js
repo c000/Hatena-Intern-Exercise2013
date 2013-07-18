a745dfbd9def51232848090458209f96aee5aad1
LabArchMain <chikusat@hotmail.com>
2013-07-18 13:48:35 +0000

    Add event listener to the button

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,9 @@
 // 課題 JS-3 の実装をここに記述してください。
+(function (document, undefined) {
+	'use strict';
+
+	var submitButtom = document.getElementById ("submit-button");
+	submitButtom.addEventListener ("click", function () {
+	});
+	console.log (submitButtom);
+})(document, undefined);
